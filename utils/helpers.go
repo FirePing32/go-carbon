831c2735b87eb0c2ceff0d8703e4236f72ac41c4
Prakhar Gurunani <prakhargurunani@gmail.com>
2022-11-27 01:25:49 +0530

    user new JSON decoder

---
+++ utils/helpers.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func GetJson(url string, target interface{}) error {
+    r, err := http.Get(url)
+    if err != nil {
+        return err
+    }
+    defer r.Body.Close()
+
+    return json.NewDecoder(r.Body).Decode(target)
+}
+
+func GetCode() {
+
+}
